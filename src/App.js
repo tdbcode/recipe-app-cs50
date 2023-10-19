91b37899c35cff62b23a7e13ea68f908c117722c
Thomas Brady <thomasbrady87@gmail.com>
2023-10-19 14:49:49 -0500

    update

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ function App() {
   const [searchcriteria, setSearchCriteria] = useState(""); 
   const [healthcriteria, setHealthCriteria] = useState("");
   const [mealtypecriteria, setMealTypeCriteria] = useState(''); 
-  const [dietcriteria, setDietCriteria] = useState('');
+  const [dietcriteria, setDietCriteria] = useState("");
   const [recipes, setRecipes] = useState([]);
   const [HEALTH_URL, setHealthURL] = useState("");
   const [DIET_URL, setDietURL] = useState("");
   const [MEALTYPE_URL, setMealTypeURL] = useState("");
-  const RECIPE_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchcriteria}&app_id=${API.APP_ID}&app_key=${API.APP_KEY}${HEALTH_URL}${DIET_URL}${MEALTYPE_URL}`;
+  const [mincalories, setMinCalories] = useState('');
+  const [maxcalories, setMaxCalories] = useState('');
+  const [CALORIES_URL, setCaloriesURL] = useState("");
+  const RECIPE_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchcriteria}&app_id=${API.APP_ID}&app_key=${API.APP_KEY}${HEALTH_URL}${DIET_URL}${MEALTYPE_URL}${CALORIES_URL}`;
 
 
   // https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=0b90b246&app_key=f08b044bcfbbe977ae670414e62303c8&diet=low-fat&health=celery-free&mealType=Dinner
@@ -26,21 +29,32 @@ function App() {
     e.preventDefault();
     getRecipes();
     console.log(RECIPE_URL); 
-  }
+  };
+
+  const filterByCalories = (items) => {
+    const newRecipeList = items.filter(() => {
+      const calories = items.recipe.calories;
+      if (calories>=mincalories && calories <=maxcalories){
+        return true;
+      }
+    })
+    console.log(newRecipeList);
+  };
  
   const getRecipes = async () => {
-    const response = await axios.get(RECIPE_URL)
-    .then((response) => {
-       console.log("GET Response");
-       console.log(response.data.hits);
-       setRecipes(response.data.hits);
-    })
-    .catch((error) => {
-      console.log("Error in fetching recipes.")
-    })
-    .finally(() => {
+    try{
+      const response = await axios.get(RECIPE_URL)
+      console.log("GET Response");
+      console.log(response.data.hits);
+      filterByCalories(response.data.hits);
+      setRecipes(response.data.hits);
+    }
+    catch (error) {
+      console.log("Error in fetching recipes.", error)
+    }
+    finally{
       console.log("Promise completed.");
-    });
+    };
   };
 
   const handleHealthChange = (h) => {
@@ -79,8 +93,39 @@ function App() {
     else{
       setMealTypeURL(`&mealType=${newMealTypeCriteria}`);
     }
-    
   };
+
+  const handleMinCalories = (c) => {
+    const mincals = c.target.value;
+    setMinCalories(mincals);
+
+    if (mincals === "" && maxcalories === "") {
+      setCaloriesURL(""); // Reset the URL if both fields are empty
+    } else if (maxcalories === "") {
+      setCaloriesURL(`&calories=${encodeURIComponent(mincals)}`);
+    } else if (mincals === "") {
+      setCaloriesURL(`&calories=${encodeURIComponent(maxcalories)}`);
+    } else {
+      setCaloriesURL(`&calories=${encodeURIComponent(mincals)}-${encodeURIComponent(maxcalories)}`);
+    }
+  };
+
+  const handleMaxCalories = (c) => {
+    const maxcals = c.target.value;
+    setMaxCalories(maxcals);
+
+    if (maxcals === "" && mincalories === "") {
+      setCaloriesURL(""); // Reset the URL if both fields are empty
+    } else if (mincalories === "") {
+      setCaloriesURL(`&calories=${encodeURIComponent(maxcals)}`);
+    } else if (maxcals === "") {
+      setCaloriesURL(`&calories=${encodeURIComponent(mincalories)}`);
+    } else {
+      setCaloriesURL(`&calories=${encodeURIComponent(mincalories)}-${encodeURIComponent(maxcals)}`);
+    }
+  };
+
+
 
   return (
     <div className="App">
@@ -93,7 +138,7 @@ function App() {
           value={searchcriteria} 
           onChange={(s) => setSearchCriteria(s.target.value)}
         />
-        <input className="App-form-submit" type="Submit"/>
+        
         <select className="App-food-options" value={healthcriteria} onChange={handleHealthChange}>
         <option value="">Select a health option</option>
           {HealthLabels.HealthLabels.map((healthLabel) => (
@@ -119,7 +164,21 @@ function App() {
         ))}
       </select>
 
+      <input 
+          type="text"
+          className="App-form-calories-bar" 
+          value={mincalories} 
+          onChange={handleMinCalories}
+        />
 
+      <input 
+          type="text"
+          className="App-form-calories-bar" 
+          value={maxcalories} 
+          onChange={handleMaxCalories}
+        />
+
+      <input className="App-form-submit" type="Submit"/>
       </form>
       {recipes.map(recipe =>(
         <Recipe 
@@ -166,4 +225,12 @@ https://stackoverflow.com/questions/17496513/json-to-dictionary-in-javascript
 Creation a dictionary from a JSON file and adding it as options to a select:
 https://stackoverflow.com/questions/40810673/mapping-over-a-dictionary-to-display-dropdown-values-in-javascript
 https://www.pluralsight.com/guides/convert-a-json-file-to-an-array-in-react
+Getting length of list:
+https://linuxhint.com/dictionary-length-javascript/
+Removing items from list:
+https://stackoverflow.com/questions/49103681/best-way-to-delete-an-item-from-dictionary-react-js#:~:text=As%20for%20deleting%20an%20object,more%20what%20the%20problem%20is.
+https://www.robinwieruch.de/react-remove-item-from-list/
+https://dev.to/collegewap/how-to-delete-an-item-from-the-state-array-in-react-kl
 */
+
+
